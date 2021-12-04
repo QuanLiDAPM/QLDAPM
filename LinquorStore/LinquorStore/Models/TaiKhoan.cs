832cd9f2428644f4ca9a5241f9f1e9580876ff36
QuocDat123 <tqdat_19pm@student.agu.edu.vn>
2021-12-04 11:16:03 +0700

    commit

--- LinquorStore/LinquorStore/Models/TaiKhoan.cs
+++ LinquorStore/LinquorStore/Models/TaiKhoan.cs
@@ -19,21 +19,33 @@ namespace LinquorStore.Models
         [Key]
         [Column("ID")]
         public int Id { get; set; }
+        [Display(Name = "Chức vụ")]
         public int ChucVu { get; set; }
+
+        [Display(Name = "Họ tên")]
         [Required]
         [StringLength(255)]
         public string HoTen { get; set; }
+
+        [Display(Name = "Tên đăng nhập")]
         [Required]
         [StringLength(255)]
         public string TenDangNhap { get; set; }
+
+        [Display(Name = "Mật khẩu")]
         [Required]
         [StringLength(255)]
         public string MatKhau { get; set; }
+
+        [Display(Name = "Số điện thoại")]
         [Required]
         [StringLength(255)]
         public string SoDienThoai { get; set; }
+
+        [Display(Name = "Hình Ảnh Bìa")]
         [StringLength(255)]
         public string HinhAnhBia { get; set; }
+
 
         [InverseProperty(nameof(DatHang.TaiKhoan))]
         public virtual ICollection<DatHang> DatHangs { get; set; }
